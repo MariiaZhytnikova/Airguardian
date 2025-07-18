b376dfbb13e18a2cfb951f8650cec50f8d7dc0fb
Mariia Zhytnikova <allusio@gmail.com>
2025-07-18 11:58:34 +0300

    database connect

---
+++ drone_db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+db_url = os.getenv("DATABASE_URL")
+
+engine = create_engine(db_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT 1;"))
+    print("✅ Connection successful. Result:", result.scalar())
